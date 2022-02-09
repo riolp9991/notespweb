b39490a1545e557f0efdea01d20e3ceb4c006a7d
RioLP <riolp9991@gmail.com>
2022-02-09 23:53:55 +0000

    feat: add the editing false

--- js/notes.js
+++ js/notes.js
@@ -1,7 +1,9 @@
+const isEditting = document.querySelector("#isEditting");
 const createButton = document.querySelector("#create");
 const float = document.querySelector(".float-form");
 createButton.addEventListener("click", () => {
 	float.classList.toggle("active");
+	isEditting.value = "false";
 });
 
 document.querySelector("#cancelCreate").addEventListener("click", (e) => {
